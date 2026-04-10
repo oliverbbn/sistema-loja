0c45d844b8154306a2f8df326fe380b1a0b6d5e4
Bruno Bernardo <bbengcivil@gmail.com>
2026-04-09 21:13:14 -0300

    aula 10 php

--- index.php
+++ index.php
@@ -7,4 +7,51 @@ $subtotal = $precoUnitario * $quantidade;
 $valorDesconto = $subtotal * $desconto;
 $valorFinal = $subtotal - $valorDesconto;
 
+
 echo "Produto: $nomeProduto | Quantidade: $quantidade | Valor Final: R$ $valorFinal";
+
+if ($quantidade == 0){
+    echo "<br>Status: Produto esgotado!";
+} else if ($quantidade > 0 && $quantidade <= 5) {
+    echo "<br>Status: Estoque crítico! Acionar fornecedor";
+
+} else {
+    echo "<br>Status: Estoque Normal";
+}
+
+if ($valorFinal > 1000.00){
+    echo "<br>Aviso: Venda de Alto Valor. Aplicar brinde especial na embalagem.<br>";
+}
+
+array($categorias = ["<br>Eletrônicos", "Móveis", "Informática", "Games", "Livros"]);
+
+//var_dump($categorias);
+
+foreach ($categorias as $categoria) {
+    echo "<br>Categoria disponível: $categoria<br>";
+}
+
+$estado = "AM";
+
+if ($estado == "SP"){
+    $frete = 15.00;
+} else if ($estado == "RJ" || $estado == "MG") {
+    $frete = 20.00;
+} else {
+    $frete = 50.00;
+}
+
+echo "<br><br> Para o estado $estado, o valor do frete é R$ $frete.<br>";
+
+array($notas_aluno = [7.5, 8.0, 6.5]);
+$soma = $notas_aluno[0]+$notas_aluno[1] + $notas_aluno[2];
+$media = $soma/count($notas_aluno);
+$mediaFormatada = number_format($media,2,'.','');
+echo " <br><br> A média do Aluno foi de $mediaFormatada.<br>";
+if ($media >= 7){
+    echo "<br>Aluno Aprovado!";
+} else if ($media >= 5 && $media <= 6.9) {
+    echo "<br>Aluno em Recuperação.";
+} else {
+    echo "<br>Aluno reprovado.";
+}
